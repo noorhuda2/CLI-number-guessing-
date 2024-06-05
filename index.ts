b21667b65a7057c2b3bce105e59f6e582230df3e
Noor ul huda <163464952+noorhuda2@users.noreply.github.com>
2024-06-05 20:21:27 +0500

    first commit

--- index.ts
+++ index.ts
@@ -2,19 +2,46 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
-const randomNumber = Math.floor(Math.random() * 6 + 1);
-
-const answers = await inquirer.prompt([
+const range = await inquirer.prompt([
   {
-    name: "userGuessedNumber",
+    message: chalk.blue("Please enter min-range for computer to randomly select from?"),
     type: "number",
-
-    message: chalk.yellow("Please guess a number between 1-6:"),
+    name: "min",
+  },
+  {
+    message: chalk.blue("Please enter max-range for computer to randomly select from?"),
+    type: "number",
+    name: "max",
   },
 ]);
 
-if (answers.userGuessedNumber === randomNumber) {
-  console.log(chalk.blueBright("Congratulations! You guessed rigth number."));
+const randomNumber =
+  Math.floor(Math.random() * (range.max - range.min + 1)) + range.min;
+const userGuessNumber = await inquirer.prompt([
+  {
+    type: "number",
+    name: "userGuessdNumber",
+    message: chalk.yellow("Guess a number between " + range.min + " to " + range.max + "\n"),
+  },
+]);
+
+if (
+  userGuessNumber.userGuessdNumber < range.min ||
+  userGuessNumber.userGuessdNumber > range.max
+) {
+  console.log(chalk.red("Your guess ouside the range you defined. Please try again."));
+} else if (userGuessNumber.userGuessdNumber === randomNumber) {
+  console.log(chalk.blueBright("Congratulations! You guessed the correct number!"));
+} else if (
+  userGuessNumber.userGuessdNumber + 1 === randomNumber ||
+  userGuessNumber.userGuessdNumber - 1 === randomNumber
+) {
+  console.log(
+   chalk.green("Ohhh! you were so close....\ncomputer randomly seleted" + randomNumber)
+  );
 } else {
-  console.log(chalk.red("You guessed wrong number."));
+  console.log(
+   chalk.redBright("Sorry you guessed wrong number....\ncomputer randomly seleted" +
+      randomNumber)
+  );
 }
